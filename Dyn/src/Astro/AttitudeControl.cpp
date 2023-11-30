1dd4aa53826ae9abd030da2021ca41dcd9e463f2
L ZZW F <zzwemail@zzwdomain.com>
2023-11-30 21:24:10 +0800

    添加了初始速率阻尼控制

--- Dyn/src/Astro/AttitudeControl.cpp
+++ Dyn/src/Astro/AttitudeControl.cpp
@@ -1,22 +1,14 @@
-#include"Astro/AttitudeControl.h"
+#include "AStro/AttitudeControl.h"
 
-
-//初始速率阻尼控制率设计
 AttitudeControl::AttitudeControl()
 {
+	workmode = 1;
 	Kp = 3;
 }
-double ControlCommand(Eigen::Vector3d _Data, Eigen::Matrix3d _InstallMatrix)
+Eigen::Vector3d AttitudeControl::ControlCommand(Eigen::Vector3d _Data, Eigen::Matrix3d _InstallMatrix)
 {
-	Eigen::Matrix3d M_inv = _InstallMatrix.inverse();
+	Eigen::Matrix3d I_inverse = _InstallMatrix.inverse();
 	Eigen::Vector3d W = -_Data;
-	Tcontrol = Kp * M_inv * W;
+	Tcontrol = Kp * I_inverse * W;
+	return Tcontrol;
 }
-//对日捕获与定向控制率设计
-
-
-//对地捕获与定向控制率设计
-
-
-
-
