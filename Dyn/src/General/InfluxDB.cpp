e79f686116ebe4a3312d9846407dd34040af53fc
‘Amadeus’ <ldx1030407672@gmail.com>
2023-12-26 11:54:06 +0800

    添加了载入数据库的功能

--- Dyn/src/General/InfluxDB.cpp
+++ Dyn/src/General/InfluxDB.cpp
@@ -1,21 +1,21 @@
 #include"General/InfluxDB.h"
 #include"General/SimTime.h"
-#include <sstream>
-CInfluxDB::CInfluxDB(const std::string& host, int port, const std::string& db)
-    : serverAddress(host), serverPort(port), dbName(db) {
+CInfluxDB::CInfluxDB(const std::string& host, int port, const std::string& dbname, const std::string& meas)
+    : dbName(dbname) {
+    setMeasurement(meas);
     WSADATA wsaData;
     // 初始化Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "Failed. Error Code : " << WSAGetLastError();
     }
     // 创建 socket
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR) {
+    if ((s = socket(PF_INET, SOCK_DGRAM, 0)) == SOCKET_ERROR) {
         std::cerr << "socket() failed with error code : " << WSAGetLastError();
     }
     // 设置服务器地址
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr("127.0.0.1"); // 本地 InfluxDB 地址
-    server.sin_port = htons(port);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = inet_addr(host.c_str()); // 本地 InfluxDB 地址
+    serverAddr.sin_port = htons(port);
     LastRenewTime = GetTimeStampMs();
 }
 
@@ -27,11 +27,9 @@ CInfluxDB::~CInfluxDB()
 
 void CInfluxDB::setMeasurement(const std::string& measurement)
 {
-    str1 = measurement;
-    str2 = str1 + ",tag=success SIM000=0.0";
-    // InfluxDB-UDP-API发送数据格式，时间戳可加可不加
-    // 标签值用于索引
-    // "measurement,tag1=value1,tag2=value2 field1=value1,field2=value2 timestamp"
+    str1.clear();
+    str1 = measurement + " SIM000=0.0";
+    //"measurement,tag1=value1,tag2=value2 field1=value1,field2=value2 timestamp"
 }
 
 void CInfluxDB::printMeasurement() const {
@@ -44,11 +42,6 @@ void CInfluxDB::printMeasurement() const {
 }
 
 
-void CInfluxDB::addKeyValue(const std::string& field, const double& value)
-{
-    str2 += "," + field + "=" + std::to_string(value);
-}
-
 void CInfluxDB::printStr2() const
 {
     std::cout << "str2: " << str2 << std::endl;
@@ -56,24 +49,18 @@ void CInfluxDB::printStr2() const
 
 void CInfluxDB::sendUdp()
 {
-    if (sendto(s, str2.c_str(), (int)str2.length(), 0, (struct sockaddr*)&server, sizeof(server)) == SOCKET_ERROR) {
+    if (sendto(s, str2.c_str(), (int)str2.length(), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
         std::cerr << "sendto() failed with error code : " << WSAGetLastError();
     }
     else {
         std::cout << "Message sent successfully." << std::endl;
     }
-    // 更新发送时刻
+    ResetStr2();
 }
 
-void CInfluxDB::clearStr2() {
-    str2 = str1 + ",";
-}
-
-std::string CInfluxDB::doubleToString(const double& value) const
-{
-    std::ostringstream oss;
-    oss << value;
-    return oss.str();
+void CInfluxDB::ResetStr2() {
+    str2.clear();
+    str2 = str1;
 }
 
 bool CInfluxDB::IsSend(double Period) const
